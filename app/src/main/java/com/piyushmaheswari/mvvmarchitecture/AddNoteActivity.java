0027604483c4680645f8045dbf700a3afb4cc4d2
Piyush2769 <startcheckingyourmail@gmail.com>
2019-08-29 16:39:23 +0530

    Added new

---
+++ app/src/main/java/com/piyushmaheswari/mvvmarchitecture/AddNoteActivity.java
@@ -0,0 +1,84 @@
+package com.piyushmaheswari.mvvmarchitecture;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    public static final String EXTRA_TITLE
+            ="com.piyushmaheswari.mvvmarchitecture.EXTRA_TITLE";
+    public static final String EXTRA_DESCRIPTION
+            ="com.piyushmaheswari.mvvmarchitecture.EXTRA_DESCRIPTION";
+    public static final String EXTRA_PRIORITY
+            ="com.piyushmaheswari.mvvmarchitecture.EXTRA_PRIORITY";
+
+
+    private EditText editTextTitle;
+    private EditText editTextDesc;
+    private NumberPicker numberPickerPri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+
+        editTextTitle=findViewById(R.id.edit_text_title);
+        editTextDesc=findViewById(R.id.edit_text_description);
+        numberPickerPri=findViewById(R.id.numberpickerPri);
+
+        numberPickerPri.setMaxValue(10);
+        numberPickerPri.setMinValue(1);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black_24dp);
+        setTitle("Add Note");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId()==R.id.saveNote)
+        {
+            saveNote();
+            return true;
+        }
+        else
+            return super.onOptionsItemSelected(item);
+    }
+
+    private void saveNote() {
+
+        String title=editTextTitle.getText().toString();
+        String descr=editTextDesc.getText().toString();
+        int priority=numberPickerPri.getValue();
+
+        if(title.trim().isEmpty() || descr.trim().isEmpty()) {
+            Toast.makeText(this, "Please enter value in Title and Description", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent data=new Intent();
+        data.putExtra(EXTRA_TITLE,title);
+        data.putExtra(EXTRA_DESCRIPTION,descr);
+        data.putExtra(EXTRA_PRIORITY,priority);
+
+        setResult(RESULT_OK,data);
+        finish();
+
+
+    }
+}
--- app/src/main/java/com/piyushmaheswari/mvvmarchitecture/MainActivity.java
+++ app/src/main/java/com/piyushmaheswari/mvvmarchitecture/MainActivity.java
@@ -2,22 +2,37 @@ package com.piyushmaheswari.mvvmarchitecture;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public static final int ADD_NEW_REQUEST=1;
     private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton floatingActionButton=findViewById(R.id.add_new_note);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,AddNoteActivity.class);
+                startActivityForResult(intent,ADD_NEW_REQUEST);
+            }
+        });
 
         RecyclerView recyclerView=findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -33,6 +48,29 @@ public class MainActivity extends AppCompatActivity {
                 noteAdapter.setNoteList(notes);
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==ADD_NEW_REQUEST && resultCode==RESULT_OK)
+        {
+            String title=data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+            String description=data.getStringExtra(AddNoteActivity.EXTRA_DESCRIPTION);
+            int priority=data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY,1);
+
+            Note note=new Note(title,description,priority);
+            noteViewModel.insert(note);
+
+            Toast.makeText(this, "Note Saved!", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "Note not saved", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
