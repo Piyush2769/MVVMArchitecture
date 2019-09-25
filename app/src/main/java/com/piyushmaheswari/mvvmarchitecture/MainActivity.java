887a8c1929854b426702f3088123b83d8eac314d
Piyush2769 <startcheckingyourmail@gmail.com>
2019-09-26 00:47:50 +0530

    Added new+1

--- app/src/main/java/com/piyushmaheswari/mvvmarchitecture/MainActivity.java
+++ app/src/main/java/com/piyushmaheswari/mvvmarchitecture/MainActivity.java
@@ -3,6 +3,7 @@ package com.piyushmaheswari.mvvmarchitecture;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -15,11 +16,18 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+
+    private TextView d;
+    private Image i;
 
 
     public static final int ADD_NEW_REQUEST=1;
